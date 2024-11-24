54764e4f2cdbe1fd324ebfe8d3c0c0796ee13ef6
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-11-24 17:17:26 +0530

    component created

--- day-three/src/App.js
+++ day-three/src/App.js
@@ -1,15 +1,21 @@
-
+import React,{useState} from 'react';
 import './App.css';
-
+import Main from './component/Main';
 function App() {
+
+  const [word,setWord]=React.useState('Sushmitha');
+  
+  
+
   return (
     <div className="App">
-     
       
+     
+      <Main message={word}/>
+     
     </div>
   );
 }
-
 
 
 export default App;
---
+++ day-three/src/component/Main.js
@@ -0,0 +1,7 @@
+export default function Main(props){
+    return(
+        <div>
+             <h1>{props.message}</h1>
+        </div>
+    )
+}
